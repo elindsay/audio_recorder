ce6250f98bc4417cd171126d4775e4cb7c7389dc
Emma Lindsay <lizardfiz@gmail.com>
2023-04-11 21:45:16 -0700

    Working, switching to simpleaudio

--- audio_main.py
+++ audio_main.py
@@ -6,16 +6,17 @@ from audio_player import play_loop
 from audio_recorder import AudioRecorder
 from playsound import playsound
 import RPi.GPIO as GPIO
+import simpleaudio as sa
 
 ar = AudioRecorder()
 
+global p
 p = multiprocessing.Process(target=play_loop)
 
 def read_keystrokes():
     while True:
         key = keyboard.read_event()
         print(key)
-        global p
         p.terminate()
         time.sleep(1)
         #p = playsound("audio_questions/question.wav")
@@ -38,30 +39,37 @@ ar = AudioRecorder()
 
 def read_gpio():
     last_state = GPIO.input(18)
+    #global p
+    #p.start()
     while True:
         state = GPIO.input(18)
         if(last_state != state):
             last_state = state
             print("Button pushed") 
-            global p
             p.terminate()
-            p = multiprocessing.Process(target=playsound, args=("audio_questions/question.wav",))
-            p.start()
-            time.sleep(8)
-            p.terminate()
+            playsound("audio_questions/question.wav")
+            #p = multiprocessing.Process(target=playsound, args=("audio_questions/question.wav",))
+            #p.start()
+            #time.sleep(8)
+            #p.terminate()
             ar.record_audio()
             filename ="audio_answers/"+ ar.get_last_filename()
             print(filename)
-            p = multiprocessing.Process(target=playsound, args=(filename,))
-            p.start()
-            time.sleep(15)
+            #p = multiprocessing.Process(target=playsound, args=(filename,))
+            #p.start()
+            playsound(filename)
             p.terminate()
             p = multiprocessing.Process(target=play_loop)
             p.start()
 
 #threading.Thread(target = play_loop).start()
 #threading.Thread(target = read_keystrokes).start()
-threading.Thread(target = read_gpio).start()
-if __name__ == '__main__':
-    #time.sleep(1)
-    p.start()
+#threading.Thread(target = read_gpio).start()
+wave_obj = sa.WaveObject.from_wave_file("audio_questions/question.wav")
+play_obj = wave_obj.play()
+time.sleep(2)
+play_obj.stop()
+#play_obj.wait_done()
+#if __name__ == '__main__':
+#    #time.sleep(1)
+#    read_gpio()
